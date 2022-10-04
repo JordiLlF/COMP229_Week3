0967479c04f4e3a7285b2f3c35f0fade6ab5f833
Jordi Llobet <jllobetf@outlook.com>
2022-10-04 00:34:34 -0400

    Completed Third-Party Express Middleware activation

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-// import express
+// Third Party Modules
 import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import session from "express-session";
+
+// ES Modules fix for __dirname
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 // instantiate app-server
 const app = express();
+
+
+// setup ViewEngine EJS
+app.set("views", path.join(__dirname,"/views"));
+app.set("view engine", "ejs");
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieparser());
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(session({
+    secret: "MySecret",
+    saveUninitialized: false,
+    resave: false
+}))
+
 
 // custom middleware
 function helloWorld(req, res, next){
